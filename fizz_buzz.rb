b55c3d312a573d4a0a2fd7da82ad76a746c84694
KoichiOzaki <goingmyway19851224@gmail.com>
2017-08-29 19:18:52 +0900

    first commit

---
+++ fizz_buzz.rb
@@ -0,0 +1,32 @@
+def fizz_buzz?(n)
+  return "FizzBuzz" if n % 15 == 0
+  return "Fizz" if n % 3 == 0
+  return "Buzz" if n % 5 == 0
+  return n
+end
+
+# 通常ループver
+def loop_fizz_buzz(n)
+  (1..n).each {|i| puts fizz_buzz?(i)}
+end
+
+# 配列ver
+def array_fizz_buzz(n)
+  array = [*(1..n)]
+  array.map {|i| puts fizz_buzz?(i)}
+end
+
+# 再帰処理ver
+def recursive_puts_fizzbuzz(n)
+  recursive_puts_fizzbuzz(n-1) if n > 1
+  puts fizz_buzz?(n)
+end
+
+# 再帰処理ver
+# recursive_puts_fizzbuzz(100)
+
+# 配列ver
+# array_fizz_buzz(100)
+
+# 通常ループver
+loop_fizz_buzz(100)
